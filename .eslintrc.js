a5e7ad50f3f8aaea5d88fd9f1720d81694e1d8a4
markschuttehva <45555455+markschuttehva@users.noreply.github.com>
2019-05-17 11:35:03 +0200

    First upload

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "semi": [2, "always"],
+        "quotes": [2, "double"],
+        "no-var": [2],
+        "arrow-body-style": [2, "as-needed"],
+        "prefer-arrow-callback": 2,
+        "prefer-destructuring": 1,
+        "new-cap": ["error", {"newIsCap": false}],
+        "space-before-function-paren": [2, "never"],
+        "space-before-blocks": [2, "always"]
+    }
+};
+
+//https://eslint.org/docs/rules/
